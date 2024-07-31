fabf4d4125b9e2309697871816f172dd0676e6d0
Ivan Chuklaykin <chuklaykin@yandex.ru>
2024-07-31 14:10:28 +0300

    fix first CORS  error

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from contextlib import asynccontextmanager
 from datebase import create_tables, delete_tables
 from schemas import SCandidateAdd
 from router import router as tasks_router
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,5 +13,20 @@ async def lifespan(app: FastAPI):
     await create_tables()
     yield
 
+
 app = FastAPI(lifespan=lifespan)
+
+allowed_origins = [
+    "http://localhost",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(tasks_router)
